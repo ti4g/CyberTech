d8121f113f503c500d32e0e865e15b2346177456
ti4g <tiagosousaquixabaaa@gmail.com>
2025-02-25 20:33:09 -0300

    nav bar para pequenas telas

--- js/config.js
+++ js/config.js
@@ -1,6 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     const isConfigPage = window.location.pathname.includes('config.html');
 
+    // nav bar para aparecer em telas pequenas
+    
+    document.addEventListener('DOMContentLoaded', () => {
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    
+        // Add a click event on each of them
+        if ($navbarBurgers.length > 0) {
+            $navbarBurgers.forEach(el => {
+                el.addEventListener('click', () => {
+                    // Get the target from the "data-target" attribute
+                    const target = el.dataset.target;
+                    const $target = document.getElementById(target);
+    
+                    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                    el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
+                });
+            });
+        }
+    });
+
+
     // ==================== SISTEMA DE TRADUÇÕES ====================
     const traducoes = {
         'pt-br': {
